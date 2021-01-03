954c21442d5ec0fd05018f667b3d2a45d992e7b8
Raphael Tang <raphpb1912@gmail.com>
2021-01-03 21:44:36 +0800

    Added indoc function

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * # Indented Documents
+ *
+ * Template literal function to generate unindented strings
+ *
+ * @example
+ * indoc`
+ *   test
+ * `
+ * // output: `test`
+ */
+export function indoc(document: TemplateStringsArray): string {
+  return document[0]
+    .split("\n")
+    .map((item) => item.trim())
+    .filter((item) => item.length > 0)
+    .join("\n")
+}
+
+export default indoc

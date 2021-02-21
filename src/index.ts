b6f9874e02b12a193341a0998603adb75da9c16d
Raphael Tang <raphpb1912@gmail.com>
2021-02-21 18:13:39 +0800

    Remove empty lines thing

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ export function indoc(document: TemplateStringsArray): string {
   return document[0]
     .split("\n")
     .map((item) => item.trim())
-    .filter((item) => item.length > 0)
     .join("\n")
 }
 
@@ -21,7 +20,6 @@ export function ind(document: string): string {
   return document
     .split("\n")
     .map((item) => item.trim())
-    .filter((item) => item.length > 0)
     .join("\n")
 }
 
